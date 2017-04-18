f4dbd03ef65dfcd67ef650b3d90f48f4f35aeae7
liujingquan <1275169741@qq.com>
2017-04-18 23:00:25 +0800

    add git editor

--- gitcbeditor.h
+++ gitcbeditor.h
@@ -1,1 +1,17 @@
+#ifndef GITCBEDITOR_H
+#define GITCBEDITOR_H
 
+#include<logger.h>
+
+class gitcbeditor
+{
+public:
+    gitcbeditor();
+    ~gitcbeditor();
+    void Append();
+    void Clear();
+    void Copy();
+
+};
+
+#endif // GITCBEDITOR_H
